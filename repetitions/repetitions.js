973f995fb72772e26766b290f8060c951e71fcbd
feliciaeklund <feliciaeklund9@gmail.com>
2025-01-12 18:45:17 +0100

    Funktioner för repetitions-sidan
    Har skapat två olika funktioner, en för att hitta de mest repeterade nummer i griden och den andra funktionen är till för att hitta de nummer i griden som inte finns.

--- repetitions/repetitions.js
+++ repetitions/repetitions.js
@@ -1,3 +1,17 @@
     const gridContainer = document.getElementById("numbergrid");
     const repInputNumber = document.getElementById("repinputnumber");
     const repInput = document.getElementById("repinput");
+
+    const findMostRepeatedNumbers = () => {
+        const allCells = document.querySelectorAll('.gridCell');
+        const numberCounts = {};
+        const repeatedNumbers = [];
+        let totalRepetitions = 0;
+
+
+    const findNumbersNotInPlace = () => {
+        const allCells = document.querySelectorAll('.gridCell');
+        const gridNumbers = new Set();
+
+        allCells.forEach(cell => gridNumbers.add(parseInt(cell.textContent)));
+
