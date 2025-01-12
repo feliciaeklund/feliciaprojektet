82843803dc0378d9fdf649e97fe81f3efe764268
feliciaeklund <feliciaeklund9@gmail.com>
2025-01-12 19:13:35 +0100

    Deklarerat variabler

---
+++ addUpTo/addUpTo.js
@@ -0,0 +1,4 @@
+    const gridContainer = document.getElementById("numbergrid");
+    const addupInput = document.getElementById("addupinput");
+    const findButton = document.getElementById("findButton");
+
